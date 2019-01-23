b128029e3cc70517779360aeb6dc938f76dd651b
t-tullis <t.tullis34@gmail.com>
2019-01-23 15:00:36 -0800

    finished challenges

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -14,6 +14,15 @@
   * dimensions (These represent the character's size in the video game)
   * destroy() // prototype method -> returns the string: 'Object was removed from the game.'
 */
+function GameObject(attrs){
+  this.createdAt = new Date();
+  this.dimensions = attrs.dimensions;
+}
+//GameObject Prototype
+GameObject.prototype.destroy =  function(){
+  return `${this.name} was removed from the game`
+};
+
 
 /*
   === CharacterStats ===
@@ -22,6 +31,21 @@
   * takeDamage() // prototype method -> returns the string '<object name> took damage.'
   * should inherit destroy() from GameObject's prototype
 */
+function CharacterStats(charAttrs){
+  GameObject.call(this, charAttrs)
+  this.healthPoints = charAttrs.healthPoints;
+  this.name= charAttrs.name;
+}
+
+CharacterStats.prototype = Object.create(GameObject.prototype);
+
+//CharacterStats prototype
+CharacterStats.prototype.takeDamage = function(){
+  return `${this.name} took damage`
+}
+
+
+
 
 /*
   === Humanoid (Having an appearance or character resembling that of a human.) ===
@@ -32,7 +56,21 @@
   * should inherit destroy() from GameObject through CharacterStats
   * should inherit takeDamage() from CharacterStats
 */
- 
+function Humanoid(humanAttrs){
+  GameObject.call(this, humanAttrs)
+  CharacterStats.call(this, humanAttrs)
+  this.team = humanAttrs.team;
+  this.weapons = humanAttrs.weapons;
+  this.language = humanAttrs.language;
+}
+Humanoid.prototype = Object.create(GameObject.prototype);
+Humanoid.prototype = Object.create(CharacterStats.prototype);
+
+//Humanoid prototype
+Humanoid.prototype.greet = function(){
+  return `${this.name} offers a greeting in ${this.language}`
+}
+
 /*
   * Inheritance chain: GameObject -> CharacterStats -> Humanoid
   * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
@@ -41,7 +79,7 @@
 
 // Test you work by un-commenting these 3 objects and the list of console logs below:
 
-/*
+
   const mage = new Humanoid({
     createdAt: new Date(),
     dimensions: {
@@ -102,7 +140,7 @@
   console.log(archer.greet()); // Lilith offers a greeting in Elvish.
   console.log(mage.takeDamage()); // Bruce took damage.
   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-*/
+
 
   // Stretch task: 
   // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
--- assignments/this.js
+++ assignments/this.js
@@ -1,26 +1,81 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
-*
+* 1. Window/Global - When the value of "This" is attached globally.
+* 2. Implicit - Whenever a function is called by a dot. The object to the left of the dot is what "This" is referring to.
+* 3. new binding - When a constructor function is used this refers to the instance of the object that is created
+*4. explicit - Whenever JavaScript’s call or apply method is used, this is explicitly defined.
+
 * write out a code example of each explanation above
 */
 
 // Principle 1
 
 // code example for Window Binding
+function sayHey(name){
+    console.log(this)
+    return name;
+}
+sayHey('Terrell')
 
 // Principle 2
 
 // code example for Implicit Binding
 
+const myObj = {
+    greeting: 'Whats up?',
+    sayWhatsUp: function(name) {
+      console.log(`${this.greeting} My name is ${name}`);
+      console.log(this);
+    }
+  };
+  myObj.sayWhatsUp('Terrell');
 // Principle 3
 
 // code example for New Binding
+function student(name) {
+    this.action = 'raised their hand ';
+    this.student = name;
+    this.raiseHand = function() {
+      console.log(`${this.student}, ${this.action}`);
+      console.log(this);
+    };
+  }
+
+  const terrell = new student('Terrell')
+  terrell.raiseHand();
+
 
 // Principle 4
 
 // code example for Explicit Binding
+//Parent Constructor
+function Parent(attributes){
+    this.age = attributes.age;
+    this.location = attributes.location;
+    this.name = attributes.name;
+    this.phrase = attributes.phrase;
+  }
+  
+  Object.prototype.speak = function(){
+    return (`${this.phrase}`)
+  }
+
+  //New child constructor
+  function Child(attributes){
+      //explicit biding
+    Parent.call(this, attributes)
+      this.play = attributes.play
+  }
+
+  let kiddo1 = new Child({
+      age: '1month',
+      location: 'Oakland',
+      name: 'Koah',
+      play: 'Plays with feet',
+      phrase: 'Goooo Gooo'
+  })
+
+  console.log(kiddo1.speak());
+  console.log(kiddo1.name);
+
